2eef3dea771e64d2251d0fb59fce33d3bf54409a
Wade Sellers <Waderoni@gmail.com>
2014-10-13 13:44:54 -0500

    Initial Commit

---
+++ MeetUpApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MeetUpApp
+//
+//  Created by Wade Sellers on 10/13/14.
+//  Copyright (c) 2014 Wade Sellers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MeetUpAppTests/MeetUpAppTests.m
@@ -0,0 +1,40 @@
+//
+//  MeetUpAppTests.m
+//  MeetUpAppTests
+//
+//  Created by Wade Sellers on 10/13/14.
+//  Copyright (c) 2014 Wade Sellers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface MeetUpAppTests : XCTestCase
+
+@end
+
+@implementation MeetUpAppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
